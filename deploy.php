1e607911b64bed51a2646d1e2c2819d1233ad1b8
Shahaf Antwarg <santwarg@gmail.com>
2017-10-14 15:48:21 +0300

    intiial

--- deploy.php
+++ deploy.php
@@ -3,38 +3,13 @@ namespace Deployer;
 
 require 'recipe/laravel.php';
 
-// Project name
-set('application', 'tempcode.xyz/meetup');
-
 // Project repository
 set('repository', 'https://github.com/shahafan/laravel');
-
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
-
-// Shared files/dirs between deploys 
-set('shared_files', ['.env']);
-add('shared_dirs', []);
-
-// Writable dirs by web server 
-add('writable_dirs', []);
-set('allow_anonymous_stats', false);
 
 // Hosts
 
 host('tempcode.xyz')
-    ->set('deploy_path', '~/{{application}}');    
-    
-// Tasks
+    ->user('shahaf')
+    ->set('deploy_path', '/home/shahaf/tempcode.xyz/laravel');
 
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
-
-// [Optional] if deploy fails automatically unlock.
-after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'artisan:migrate');
-
+// after('deploy:update_code', 'artisan:migrate');
