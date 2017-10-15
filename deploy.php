d6e980923f6c36ea057b79b912811320ea8c90d4
Shahaf Antwarg <santwarg@gmail.com>
2017-10-15 13:11:52 +0300

    reload php fpm on deploy

--- deploy.php
+++ deploy.php
@@ -8,8 +8,16 @@ set('repository', 'https://github.com/shahafan/laravel-in-production');
 
 // Hosts
 
-host('tempcode.xyz')
+host('45.77.140.99')
     ->user('shahaf')
     ->set('deploy_path', '/home/shahaf/tempcode.xyz/laravel');
 
 // after('deploy:update_code', 'artisan:migrate');
+
+task('reload:php-fpm', function () {
+  // Attention: The user must have rights for restart service
+  // visudo: shahaf  ALL=NOPASSWD:/usr/sbin/service php7.0-fpm restart
+  run('sudo /usr/sbin/service php7.0-fpm restart');
+});
+
+after('deploy', 'reload:php-fpm');
