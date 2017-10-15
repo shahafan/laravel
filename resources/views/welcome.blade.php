239c783acc74b4d7ed67354ab5e0e6e5c77f34a1
Shahaf Antwarg <santwarg@gmail.com>
2017-10-15 12:19:10 +0300

    intiial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test
+                    Laravel In Production
                 </div>
 
                 <div class="links">
